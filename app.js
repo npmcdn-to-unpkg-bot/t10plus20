0e9f1f5d86c0315bf9beb58fba7dd57653d67937
Michael Theimer <michael.theimer@eniro.com>
2016-08-28 15:26:09 +0200

    comments function added

--- app.js
+++ app.js
@@ -34,10 +34,64 @@ app.use(stylus.middleware({
 app.use('/', express.static(__dirname + '/public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(function(req, res, next) {
+	// Set permissive CORS header - this allows this server to be used only as
+    // an API server in conjunction with something like webpack-dev-server.
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache')
+    next()
+})
 app.use(cookieParser())
 
 app.use('/', index)
 app.use('/summonthegreatdemons', satan)
+
+var COMMENTS_FILE = path.join(__dirname, 'comments.json')
+
+app.get('/api/comments', function(req, res) {
+	fs.readFile(COMMENTS_FILE, function(err, data) {
+		if (err) {
+			console.error(err)
+			process.exit(1)
+		}
+		//res.send('success')
+		res.json(JSON.parse(data))
+	})
+})
+
+app.post('/api/comments', function(req, res) {
+	fs.readFile(COMMENTS_FILE, function(err, data) {
+		if (err) {
+			console.error(err)
+			process.exit(1)
+		}
+		var comments = JSON.parse(data)
+
+		var newComment = {
+			id: Date.now(), 
+			author: req.body.author,
+			text: req.body.text
+		}
+		comments.push(newComment)
+
+		fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+			if (err) {
+				console.error(err)
+				process.exit(1)
+			}
+			res.json(comments)
+		})
+	})
+})
+
+app.use('/message', function(req, res, next) {
+	//res.setHeader('Access-Control-Allow-Origin', '*')
+	//res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	//res.setHeader('Cache-Control', 'no-cache')
+	res.sendFile('message/index.html')
+})
 
 app.use(function(req, res, next) {
 	var err = new Error('Not found')
---
+++ public/message/js/message.js
@@ -0,0 +1,134 @@
+var CommentBox = React.createClass({
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data})
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString())
+				console.warn(xhr.responseText)
+			}.bind(this)
+		})
+	},
+	handleCommentSubmit: function(comment) {
+		var comments = this.state.data
+		comment.id = Date.now()
+		var newComment = comments.concat([comment])
+		this.setState({data: newComment})
+
+		$.ajax({
+			url: this.props.url, 
+			dataType: 'json', 
+			type: 'POST', 
+			data: comment,
+			success: function(data) {
+				this.setState({data: data})
+			}.bind(this),
+			error: function(xhr, status, err) {
+				this.setState({data: comments})
+				console.error(this.props.url, status, err.toString())
+			}.bind(this)
+		})
+	},
+	getInitialState: function() {
+		return {data: []}
+	}, 
+	componentDidMount: function() {
+		this.loadCommentsFromServer()
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval)
+	}, 
+	render: function() {
+		var dataLength = this.state.data.length 
+		//console.log(dataLength)
+		if (dataLength === 0) {
+			return (
+				<div className="writeSomething">
+					<CommentForm onCommentSubmit={this.handleCommentSubmit} />
+					<pre>Be the first too write something</pre>
+				</div>
+			)
+		} else {
+			return (
+				<div className="commentBox">
+					<CommentForm onCommentSubmit={this.handleCommentSubmit} />
+					<CommentList data={this.state.data} />
+				</div>
+			)	
+		}
+		
+	}
+})
+
+var CommentForm = React.createClass({
+	getInitialState: function() {
+		return {author: '', text: ''}
+	}, 
+	handleAuthorChange: function(e) {
+		this.setState({author: e.target.value})
+	}, 
+	handleTextChange: function(e) {
+		this.setState({text: e.target.value})
+	}, 
+	handleSubmit: function(e) {
+		e.preventDefault()
+		var author = this.state.author.trim()
+		var text = this.state.text.trim()
+		if (!text || !author) {
+			return
+		}
+		this.props.onCommentSubmit({author: author, text: text})
+		this.setState({author: '', text: ''})
+	}, 
+	render: function() {
+		return (
+			<div className="commentFormContainer">
+				<h1>Message/comment/idea bord</h1>
+				<form className="commentForm" onSubmit={this.handleSubmit}>
+					<input type="text" placeholder="What's on your mind" value={this.state.text} onChange={this.handleTextChange} />
+					<input type="text" placeholder="Name" value={this.state.author} onChange={this.handleAuthorChange} />
+					<input className="formSubmit" type="submit" value="Post" />
+				</form>
+			</div>
+		)
+	}
+})
+
+var CommentList = React.createClass({
+	render: function() {
+		var that = this
+		var commentNodes = this.props.data.map(function(comment, index) {
+			return (
+				<Comment author={comment.author} key={comment.id} id={index}>
+					{comment.text}
+				</Comment>
+			)
+		})
+		return (
+			<div className="commentList">
+				{commentNodes}
+			</div>
+		)
+	}
+})
+
+var Comment = React.createClass({
+	render: function() {
+		return (
+			<div className="comment">
+				<p>
+					{this.props.children}
+				</p>
+				<h4 className="commentAuthor">
+					 - {this.props.author}
+				</h4>
+			</div>
+		)
+	}
+})
+
+ReactDOM.render(
+	<CommentBox url="/api/comments" pollInterval={2000} />, document.getElementById('content')
+)
